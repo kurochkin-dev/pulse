b83054d780f93a85cd828e45af2eb5d9e2c75676
Dmitriy <57505659+kurochkin-dev@users.noreply.github.com>
2019-12-27 01:56:52 +0300

    update,modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-      speed: 1200,
-      //adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-			      dots: true,
-			      arrows: false
-          }
-        }
-      ]
-    });
-    
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-      $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+		$('.carousel__inner').slick({
+			speed: 1200,
+			//adaptiveHeight: true,
+			prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+			nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						dots: true,
+						arrows: false
+					}
+				}
+			]
+		});
+		
+		$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+			$(this)
+				.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+				.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 	});
 
 	function toggleSlide(item) {
@@ -33,4 +33,20 @@ $(document).ready(function(){
 
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
+	
+	// Modal
+
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow')
+	});
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
 });
